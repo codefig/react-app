3156e11d2d4879cfcc593311da87d6592566f8e2
Moshood Alaran <geekyshood@gmail.com>
2019-02-14 11:42:33 +0100

    returns state auth object

--- src/reducers/AuthReducer.js
+++ src/reducers/AuthReducer.js
@@ -1,7 +1,10 @@
 const initial_state = {email : ''}
+import {EMAIL_CHANGED} from '../actions/types';
 
 export default (state = initial_state, action) => {
     switch(action.type){
+        case EMAIL_CHANGED: 
+            return {...state, email : action.payload};
         default : 
             return state;
     }
